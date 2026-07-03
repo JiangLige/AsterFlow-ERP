e4621487e6d1a80376aa5d85cab880ccc0d0e153
JiangLige <asd1701215424@163.com>
2026-07-03 11:00:12 +0800

    test: describe auth session behavior

---
+++ server/src/test/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO t_user
+    (username, password, real_name, role, status, created_at, updated_at, deleted, version)
+VALUES
+    ('admin', '$2a$10$6bszuJilQ5N3LJogrSyLE..aupUP3Neh6xvyTWwNJVzGI0uFK5tbC', '系统管理员', 'ADMIN', 'ACTIVE', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0, 0);
